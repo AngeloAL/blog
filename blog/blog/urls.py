57d763aa71e86de335e4e771d94178862554b2aa
AngeloAL <angelo37@163.com>
2022-09-08 15:57:31 +0800

    register

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+#1. 导入系统的 logging
+import logging
+# #2.创建日志器
+# logger=logging.getLogger('django')
+#
+# from django.http import HttpResponse
+# def log(request):
+#     # 3.试用日志器记录信息
+#     logger.info('info')
+#     return HttpResponse('test')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # include 参数1要设置为元组（urlconf_module, app_name）
+    # namespace 设置命名空间
+    path('', include(('users.urls', 'users'), namespace='users')),
+    # path('',log),
 ]
